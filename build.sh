9ee05d19836c8700609c255218708f354c28041d
Matt Corallo <git@bluematt.me>
2013-12-12 22:34:15 -0500

    echo Success on build success

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 $CC $CFLAGS -Wall -Werror -O2 -std=c99 shamirssecret.c -DTEST -o shamirssecret && ./shamirssecret &&
 $CC $CFLAGS -Wall -Werror -O2 -c shamirssecret.c -o shamirssecret.o &&
-$CC $CFLAGS -Wall -Werror -O2 -std=c99 main.c shamirssecret.o -o shamirssecret
+$CC $CFLAGS -Wall -Werror -O2 -std=c99 main.c shamirssecret.o -o shamirssecret &&
+echo "Success!"
